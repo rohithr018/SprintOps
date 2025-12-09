fd23fc73025dc415ae4f33b13cdc0186213cbdaa
rohithr018 <rohith018.r@gmail.com>
2025-12-09 13:07:59 +0530

    deployment fix #4

--- backend/app.js
+++ backend/app.js
@@ -19,7 +19,7 @@ const userRoutes = require("./routes/user.routes");
 const app = express();
 
 app.use(express.json());
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(helmet());
 app.use(compression());
 
